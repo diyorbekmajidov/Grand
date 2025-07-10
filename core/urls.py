f81fadfde1e3903a5f2dc6a4dc2ebd32d011f676
Diyorbek <majidovdiyorbek741@gmail.com>
2025-07-10 18:39:42 +0500

    11

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('grand.urls'))
+    path('', include('grand.urls')),
+    re_path('media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path('^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+handler404 = 'grand.views.error_404_view'
 if settings.DEBUG:
      urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
      urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
